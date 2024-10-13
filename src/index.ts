c23c84f77856c1bbbbabaaf5cd26f747f59c6f76
Sholihinmambausahmad <ahmadmambauss@gmail.com>
2024-10-13 22:57:59 +0700

    tes

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,13 @@ import { errorHandler, CustomError } from './midleware/err-handler';
 import cookieParser from "cookie-parser"
 import morgan from "morgan"
 import cors from "cors"
+
+//import DB
 import ConnectDB from './database/db';
-import bookRoutes from './routes/bookRoutes'
 
 // import Router
 import userRouter from "./routes/userRouter"
+import bookRoutes from './routes/bookRoutes'
 
 const app = express();
 const port = 3000;
@@ -19,17 +21,16 @@ ConnectDB().then(() => {
     console.error('Failed to connect to MongoDB:', err);
 });
 
+// midleware
 app.use(cors())
 app.use(express.json())
 app.use(morgan("dev"))
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }))
 
+// router
 app.use('/api/books', bookRoutes);
-
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello, TypeScript with Express!');
-});
+app.use('/api/user', userRouter)
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     errorHandler(err as CustomError, req, res, next); // Casting Error to CustomError
